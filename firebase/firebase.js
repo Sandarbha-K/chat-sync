c27219e4d61d983f3693b5d092799de1d86c88dd
Sandarbha  Komujwar <101709644+Sandarbha-K@users.noreply.github.com>
2023-07-13 12:10:36 +0530

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -5,22 +5,15 @@ import {getStorage} from "firebase/storage";
 import {getFirestore} from "firebase/firestore";
 
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyACnAY2miR4A6SFgvKmwfZ8OXEbW8gUBzU",
-//   authDomain: "fir-chat-app-73c83.firebaseapp.com",
-//   projectId: "fir-chat-app-73c83",
-//   storageBucket: "fir-chat-app-73c83.appspot.com",
-//   messagingSenderId: "595222348421",
-//   appId: "1:595222348421:web:4a3024a6b0fa933f471fec"
-// };
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDClPKKLyOBKJYVhlTvbadNTK-q6RXq2AY",
-  authDomain: "chat-app-firebase-8153a.firebaseapp.com",
-  projectId: "chat-app-firebase-8153a",
-  storageBucket: "chat-app-firebase-8153a.appspot.com",
-  messagingSenderId: "932975007460",
-  appId: "1:932975007460:web:87ec568e520d9691a4bef1"
+  apiKey: "Enter your Own Key",
+  authDomain: "Enter your Own Key",
+  projectId: "Enter your Own Key",
+  storageBucket: "Enter your Own Key",
+  messagingSenderId: "Enter your Own Key",
+  appId: "Enter your Own Key"
 };
 
 
